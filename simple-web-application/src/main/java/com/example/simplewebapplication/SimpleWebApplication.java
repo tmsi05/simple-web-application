9fb9569b7ebed6cf5e50fbef642c4d7396624da6
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 <tmsimk@gmail.com>
2022-02-09 16:01:48 +0000

    versao1

---
+++ simple-web-application/src/main/java/com/example/simplewebapplication/SimpleWebApplication.java
@@ -0,0 +1,38 @@
+package com.example.simplewebapplication;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SimpleWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWebApplication.class, args); }
+		
+		
+		
+	@Bean
+	public  CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			
+			System.out.println("Lets inspect the beans provided by the spring boot");
+			
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			
+			Arrays.sort(beanNames);
+			
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+			
+		};
+	}
+		
+		
+
+}
